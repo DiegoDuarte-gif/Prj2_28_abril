8af7f113ddb874e029a45c61755bac2fecd7a0ca
Diego <diegoduarte0n@gmail.com>
2026-04-28 20:47:55 -0300

    Prj2_28_abril

---
+++ src/App.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class App {
+    public static int somarArray(int[] lista) {
+        int soma = 0; 
+        for (int i = 0; i < lista.length; i++) {
+            soma = soma + lista[i];
+        }
+        return soma;
+    }
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite os 5 números inteiros separando apenas por espaços: ");
+        int[] numeros = new int[5];
+        for (int i = 0; i < 5; i++) {
+            numeros[i] = scanner.nextInt();
+        }
+        int resultadoSoma = somarArray(numeros);
+        System.out.println("Soma: " + resultadoSoma);
+        scanner.close();
+    }
+}
